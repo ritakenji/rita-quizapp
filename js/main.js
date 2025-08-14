63d026e0ea73fe322a3f5066159a20576ab9b470
Rita <rita.kenji@gmail.com>
2025-08-14 18:48:59 +0200

    add toggle functionality to bookmark and answer button on main.js

---
+++ js/main.js
@@ -0,0 +1,30 @@
+/* ************************************************  TOGGLE BOOKMARK
+
+ first tried .checked on bookmarkButton variable on addEventListener but this button is not the check type so decided to do it another way
+
+************************************************************************************************************************************************ */
+const bookmarkButton = document.querySelector('[data-js="cardBookmark"]');
+const imageButton = document.querySelector('[data-js="imgCardBookmark"]');
+
+bookmarkButton.addEventListener("click", () => {
+  if (!imageButton.src.includes("bookmark_filled.png")) {
+    imageButton.setAttribute("src", "./assets/bookmark_filled.png");
+  } else {
+    imageButton.setAttribute("src", "./assets/bookmark.png");
+  }
+});
+
+/* ************************************************  TOGGLE ANSWER ***************************************************************************** */
+
+const answer = document.querySelector('[data-js="answer"]');
+const cardButton = document.querySelector('[data-js="cardButton"]');
+
+cardButton.addEventListener("click", () => {
+  if (!answer.classList.contains("card__answer--hidden")) {
+    answer.classList.toggle("card__answer--hidden");
+    cardButton.innerHTML = "Show Answer";
+  } else {
+    answer.classList.toggle("card__answer--hidden");
+    cardButton.innerHTML = "Hide Answer";
+  }
+});
